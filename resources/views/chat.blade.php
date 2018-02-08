b08913f9c1533eafb9d75ab02eda6bd2c05558ea
pimenvibritania <blackbrit13@gmail.com>
2018-02-08 11:25:58 +0700

    Chat

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>pimenChat</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <h1>Chat Room</h1>
+        </div>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}">
+
+    </script>
+</body>
+</html>
