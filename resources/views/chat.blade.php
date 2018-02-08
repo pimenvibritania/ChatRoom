e03806ca592b8f4f0d1f432d95a70fbce50ef2b1
pimenvibritania <blackbrit13@gmail.com>
2018-02-08 12:42:36 +0700

    set npm

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -4,14 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>pimenChat</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
 
     <div class="container">
-        <div class="row">
-            <h1>Chat Room</h1>
+        <div class="row" id="app">
+            <ul class="list-group">
+                <li class="list-group-item active">Chat Room</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Morbi leo risus</li>
+                <li class="list-group-item">Porta ac consectetur ac</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
         </div>
     </div>
 
