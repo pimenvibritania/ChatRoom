477dd20197d358734b8f7b68e5af3dd422bcbae1
pimenvibritania <blackbrit13@gmail.com>
2018-02-09 06:35:13 +0700

    chat scroll

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -19,11 +19,12 @@
     <div class="container">
         <div class="row" id="app">
             <div class="offset-4 col-4">
-                <li class="list-group-item active">Chat Room </li>
-                <ul class="list-group">
+                <li class="list-group-item active">Chat Room by @pimenvibritania </li>
+                <ul class="list-group" v-chat-scroll>
                     
                     <message
                     v-for="value in chat.message" 
+                    :key=value.index
                     >
                         @{{ value }}
                     </message>
