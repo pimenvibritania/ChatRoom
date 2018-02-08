4c24dcbd06817231280041a913bce5b6a2891bb5
pimenvibritania <blackbrit13@gmail.com>
2018-02-08 22:43:05 +0700

    end sec 3

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -7,20 +7,30 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>pimenChat</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>
+        .list-group{
+            overflow-y: scroll;
+            height: 200px;
+        }
+    </style>
 </head>
 <body>
 
     <div class="container">
         <div class="row" id="app">
-            <ul class="list-group offset-4 col-4">
+            <div class="offset-4 col-4">
                 <li class="list-group-item active">Chat Room </li>
-                <message
-                v-for="value in chat.message" 
-                >
-                    @{{ value }}
-                </message>
+                <ul class="list-group">
+                    
+                    <message
+                    v-for="value in chat.message" 
+                    >
+                        @{{ value }}
+                    </message>
+                </ul>
                 <input type="text" class="form-control" placeholder="Type your message here..." v-model='message' @keyup.enter='send'>
-            </ul>
+
+            </div>            
         </div>
     </div>
 
