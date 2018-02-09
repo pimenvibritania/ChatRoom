0832c57b56f1028b59478ff4480278da1f2a6ee8
pimenvibritania <blackbrit13@gmail.com>
2018-02-09 09:54:39 +0700

    pusher config

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -43,13 +43,12 @@ if (token) {
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo'
+ import Echo from 'laravel-echo'
 
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key',
-//     cluster: 'mt1',
-//     encrypted: true
-// });
+ window.Pusher = require('pusher-js');
+ window.Echo = new Echo({
+     broadcaster: 'pusher',
+     key: '2c475458b9010011111d',
+     cluster: 'ap1',
+     encrypted: true
+ });
