98bfaad31bb5b9bb0e701cfb60e48cdfb0becb7c
pimenvibritania <blackbrit13@gmail.com>
2018-02-09 10:23:06 +0700

    fix

--- database/database.php
+++ database/database.php
@@ -14,18 +14,18 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
-        ],
-        'testing' => [
-            'driver'    => 'mysql',
-            'host'      => env('DB_TEST_HOST', 'localhost'),
-            'database'  => env('DB_TEST_DATABASE', 'chat_test'),
-            'username'  => env('DB_TEST_USERNAME', 'root'),
-            'password'  => env('DB_TEST_PASSWORD', ''),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-            'strict'    => false,
-        ],
+         ]
+        // 'testing' => [
+        //     'driver'    => 'mysql',
+        //     'host'      => env('DB_TEST_HOST', 'localhost'),
+        //     'database'  => env('DB_TEST_DATABASE', 'chat_test'),
+        //     'username'  => env('DB_TEST_USERNAME', 'root'),
+        //     'password'  => env('DB_TEST_PASSWORD', ''),
+        //     'charset'   => 'utf8',
+        //     'collation' => 'utf8_unicode_ci',
+        //     'prefix'    => '',
+        //     'strict'    => false,
+        // ],
     ],
     //...
 ];
